f2a84c6f947e402aea187e9310d8a631414d8ffe
Prasoon Kumar <kumarprasoon84@gmail.com>
2025-12-04 00:35:12 +0530

    Implement EventTicket contract
    Added EventTicket contract with ticket purchasing functionality.

--- Second Program.sol
+++ Second Program.sol
@@ -1,1 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
 
+contract EventTicket
+{
+    uint256 numberOfTickets;
+    uint256 ticketPrice;
+    uint256 totalAmount;
+    uint256 public startAt;
+    uint256 public endAt;
+    uint256 public timeRange;
+    string public message= "Hello everyone, buy your first event ticket";
+
+    constructor(uint256 _ticketPrice)
+    {
+        ticketPrice = _ticketPrice;
+        startAt= block.timestamp;
+        endAt= block.timestamp+ 7 days;
+        timeRange= (endAt- startAt)/60/60/24;
+    }
+
+    function buyTicket(uint256 _amount) public returns(uint256 ticketId)
+    {
+        numberOfTickets++;
+        totalAmount+= _amount;
+        ticketId = numberOfTickets;
+    }
+
+    function getTotalAmount() public view returns(uint256)
+    {
+        return totalAmount;
+    }
+}
